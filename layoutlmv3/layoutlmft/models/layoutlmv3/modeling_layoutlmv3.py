a4c3be3cdbf14087d5d05d8cbaedcb066b89e97b
wanbiguizhao <shiweishiweihubugui@live.cn>
2023-04-16 21:36:33 +0800

    PatchEmbed

--- layoutlmv3/layoutlmft/models/layoutlmv3/modeling_layoutlmv3.py
+++ layoutlmv3/layoutlmft/models/layoutlmv3/modeling_layoutlmv3.py
@@ -52,7 +52,7 @@ class PatchEmbed(nn.Module):
     """
     def __init__(self, img_size=224, patch_size=16, in_chans=3, embed_dim=768):
         super().__init__()
-        img_size = to_2tuple(img_size)
+        img_size = to_2tuple(img_size)#2=>(2,2)
         patch_size = to_2tuple(patch_size)
         self.patch_shape = (img_size[0] // patch_size[0], img_size[1] // patch_size[1])
         self.proj = nn.Conv2d(in_chans, embed_dim, kernel_size=patch_size, stride=patch_size)
@@ -62,16 +62,16 @@ class PatchEmbed(nn.Module):
         self.num_patches_h = self.patch_shape[1]
 
     def forward(self, x, position_embedding=None):
-        x = self.proj(x)
+        x = self.proj(x)#[b,c,h/patch_size,w/patch_size ]
 
         if position_embedding is not None:
             # interpolate the position embedding to the corresponding size
             position_embedding = position_embedding.view(1, self.patch_shape[0], self.patch_shape[1], -1).permute(0, 3, 1, 2)
             Hp, Wp = x.shape[2], x.shape[3]
-            position_embedding = F.interpolate(position_embedding, size=(Hp, Wp), mode='bicubic')
+            position_embedding = F.interpolate(position_embedding, size=(Hp, Wp), mode='bicubic')# 采样函数
             x = x + position_embedding
 
-        x = x.flatten(2).transpose(1, 2)
+        x = x.flatten(2).transpose(1, 2)# 从第2维度开始flatten，也就是Hp, Wp开始，把图片压平了，transpose（1，2），表示把所有的通道都放在一起处理
         return x
 
 class LayoutLMv3Embeddings(nn.Module):
